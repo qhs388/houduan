b57343f8ff7479e1d57a333df60d7693d5ae614f
刘康 <2549974870@qq.com>
2019-01-12 17:39:07 +0800

    修改后端

---
+++ node-imooc-ebook/const.js
@@ -0,0 +1,41 @@
+const resUrl = 'http://127.0.0.1:8081'
+const mp3FilePath = 'D:/nginx-1.15.7/resource/mp3'
+
+const category = [
+  'Biomedicine',
+  'BusinessandManagement',
+  'ComputerScience',
+  'EarthSciences',
+  'Economics',
+  'Engineering',
+  'Education',
+  'Environment',
+  'Geography',
+  'History',
+  'Laws',
+  'LifeSciences',
+  'Literature',
+  'SocialSciences',
+  'MaterialsScience',
+  'Mathematics',
+  'MedicineAndPublicHealth',
+  'Philosophy',
+  'Physics',
+  'PoliticalScienceAndInternationalRelations',
+  'Psychology',
+  'Statistics'
+]
+
+
+
+
+
+
+
+
+
+module.exports = {
+  resUrl,
+  category,
+  mp3FilePath
+}
