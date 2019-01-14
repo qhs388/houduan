ec8556ca1e8ba9bcb1eb4bbfcffc45e8d90f0ae5
刘康 <2549974870@qq.com>
2019-01-14 17:30:53 +0800

    添加环境变量更新

--- node-imooc-ebook/const.js
+++ node-imooc-ebook/const.js
@@ -1,5 +1,17 @@
-const resUrl = 'http://127.0.0.1:8081'
-const mp3FilePath = 'D:/nginx-1.15.7/resource/mp3'
+const env = require('./env')
+
+
+let resUrl
+let mp3FilePath
+if(env === 'dev'){
+  resUrl = 'http://127.0.0.1:8081'
+  mp3FilePath = 'D:/nginx-1.15.7/resource/mp3'
+}else if (env === 'prod'){
+  resUrl = 'http://106.14.201.210'
+  mp3FilePath = '/root/nginx/upload/mp3'
+}
+
+
 
 
 const category = [
